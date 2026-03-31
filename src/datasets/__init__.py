a870ebef7798332bcc7113e9f6690479e25f9cf4
Oleg <ryabov.ryabov-2@yandex.ru>
2026-03-31 17:36:59 +0300

    Add config for synthetic data for memory profiling

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,1 +1,2 @@
+from src.datasets.synth_dataset import SyntheticDataset
 from src.datasets.vae_dataset import VAEDataset
---
+++ src/datasets/synth_dataset.py
@@ -0,0 +1,215 @@
+import logging
+import random
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from src.utils.io_utils import ROOT_PATH, read_json
+from src.utils.model_utils import BLOCK_TYPE, get_head_key
+
+logger = logging.getLogger(__name__)
+
+
+class SyntheticDataset(Dataset):
+    """
+    Base class for the datasets.
+
+    Given a proper index (list[dict]), allows to process different datasets
+    for the same task in the identical manner. Therefore, to work with
+    several datasets, the user only have to define index in a nested class.
+    """
+
+    def __init__(
+        self,
+        length=16,
+        volume=64,
+        attr_pair_per_block=3,
+        block_data_path="src/block_data",
+    ):
+        """
+        Args:
+            index (list[dict]): list, containing dict for each element of
+                the dataset. The dict has required metadata information,
+                such as label and object path.
+            limit (int | None): if not None, limit the total number of elements
+                in the dataset to 'limit' elements.
+            shuffle_index (bool): if True, shuffle the index. Uses python
+                random package with seed 42.
+            instance_transforms (dict[Callable] | None): transforms that
+                should be applied on the instance. Depend on the
+                tensor name.
+        """
+        self.non_default_attribute_pairs = read_json(
+            ROOT_PATH / block_data_path / "non_default_attribute_pairs.json"
+        )
+
+        self.idx2block = read_json(ROOT_PATH / block_data_path / "idx2block.json")
+
+        self.length = length
+        self.volume = volume
+        self.attr_pair_per_block = attr_pair_per_block
+        assert self.attr_pair_per_block <= len(
+            self.non_default_attribute_pairs
+        ), "too much atributes requested"
+        self.instance_transforms = None
+
+    def __getitem__(self, ind):
+        """
+        Get element from the index, preprocess it, and combine it
+        into a dict.
+
+        Notice that the choice of key names is defined by the template user.
+        However, they should be consistent across dataset getitem, collate_fn,
+        loss_function forward method, and model forward method.
+
+        Args:
+            ind (int): index in the self.index list.
+        Returns:
+            instance_data (dict): dict, containing instance
+                (a single dataset element).
+        """
+
+        block_type_grid: torch.Tensor = torch.randint(
+            low=0,
+            high=len(self.idx2block),
+            size=(self.volume, self.volume, self.volume),
+            dtype=torch.long,
+        )
+
+        attributes_values = dict()
+        attributes_masks = dict()
+
+        for i, (attr, values) in enumerate(self.non_default_attribute_pairs):
+            head_key = get_head_key(attr, values)
+
+            if i < self.attr_pair_per_block:
+                mask = torch.ones_like(block_type_grid, dtype=torch.bool)
+            else:
+                mask = torch.zeros_like(block_type_grid, dtype=torch.bool)
+
+            value = torch.randint_like(
+                input=block_type_grid[mask], high=len(values), dtype=torch.long
+            )
+
+            attributes_values[head_key] = value
+            attributes_masks[head_key] = mask
+
+        instance_data = {
+            "block_type_grid": block_type_grid,
+            "attributes_values": attributes_values,
+            "attributes_masks": attributes_masks,
+            "idx": ind,
+        }
+        instance_data = self.preprocess_data(instance_data)
+
+        return instance_data
+
+    def __len__(self):
+        """
+        Get length of the dataset (length of the index).
+        """
+        return self.length
+
+    def preprocess_data(self, instance_data):
+        """
+        Preprocess data with instance transforms.
+
+        Each tensor in a dict undergoes its own transform defined by the key.
+
+        Args:
+            instance_data (dict): dict, containing instance
+                (a single dataset element).
+        Returns:
+            instance_data (dict): dict, containing instance
+                (a single dataset element) (possibly transformed via
+                instance transform).
+        """
+        if self.instance_transforms is not None:
+            for transform_name in self.instance_transforms.keys():
+                instance_data[transform_name] = self.instance_transforms[
+                    transform_name
+                ](instance_data[transform_name])
+        return instance_data
+
+    @staticmethod
+    def _filter_records_from_dataset(
+        index: list,
+    ) -> list:
+        """
+        Filter some of the elements from the dataset depending on
+        some condition.
+
+        This is not used in the example. The method should be called in
+        the __init__ before shuffling and limiting.
+
+        Args:
+            index (list[dict]): list, containing dict for each element of
+                the dataset. The dict has required metadata information,
+                such as label and object path.
+        Returns:
+            index (list[dict]): list, containing dict for each element of
+                the dataset that satisfied the condition. The dict has
+                required metadata information, such as label and object path.
+        """
+        # Filter logic
+        pass
+
+    @staticmethod
+    def _assert_index_is_valid(index):
+        """
+        Check the structure of the index and ensure it satisfies the desired
+        conditions.
+
+        Args:
+            index (list[dict]): list, containing dict for each element of
+                the dataset. The dict has required metadata information,
+                such as label and object path.
+        """
+        for entry in index:
+            assert "structire_path" in entry, (
+                "Each dataset item should include field 'structire_path'"
+                " - path to structure block and attribute data."
+            )
+
+    @staticmethod
+    def _sort_index(index):
+        """
+        Sort index via some rules.
+
+        This is not used in the example. The method should be called in
+        the __init__ before shuffling and limiting and after filtering.
+
+        Args:
+            index (list[dict]): list, containing dict for each element of
+                the dataset. The dict has required metadata information,
+                such as label and object path.
+        Returns:
+            index (list[dict]): sorted list, containing dict for each element
+                of the dataset. The dict has required metadata information,
+                such as label and object path.
+        """
+        return sorted(index, key=lambda x: x["KEY_FOR_SORTING"])
+
+    @staticmethod
+    def _shuffle_and_limit_index(index, limit, shuffle_index):
+        """
+        Shuffle elements in index and limit the total number of elements.
+
+        Args:
+            index (list[dict]): list, containing dict for each element of
+                the dataset. The dict has required metadata information,
+                such as label and object path.
+            limit (int | None): if not None, limit the total number of elements
+                in the dataset to 'limit' elements.
+            shuffle_index (bool): if True, shuffle the index. Uses python
+                random package with seed 42.
+        """
+        if shuffle_index:
+            random.seed(42)
+            random.shuffle(index)
+
+        if limit is not None:
+            index = index[:limit]
+        return index
